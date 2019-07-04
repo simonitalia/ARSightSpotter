66dd4b751acabeb06ad89cce396cac9dd6691704
Simon Italia <sitalia@magicaltomato.com>
2019-07-04 15:49:17 +0800

    Initial Commit

---
+++ ARSightSpotter/Scene.swift
@@ -0,0 +1,28 @@
+//
+//  Scene.swift
+//  ARSightSpotter
+//
+//  Created by Simon Italia on 6/24/19.
+//  Copyright © 2019 Magical Tomato. All rights reserved.
+//
+
+import SpriteKit
+import ARKit
+
+class Scene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        // Setup your scene here
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        
+        
+        
+    }
+}
